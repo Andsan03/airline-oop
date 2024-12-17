323c87406adf0d2182940164e3812b3d501b466c
Andsan03 <andrewabellosanchez123456@gmail.com>
2024-12-17 11:54:03 +0100

    components y model

---
+++ src/org/ies/airline/model/Airline.java
@@ -0,0 +1,66 @@
+package org.ies.airline.model;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Airline {
+    private String name;
+    private Flight[] flights;
+
+    public Airline(String name, Flight[] flights) {
+        this.name = name;
+        this.flights = flights;
+    }
+
+    public void showFlightsFromOrigin(String origin) {
+        for (var flight: flights) {
+            if (flight.getOrigin().equals(origin)) {
+                System.out.println(flight);
+            }
+        }
+
+    }
+
+
+    public void showInfo() {
+        System.out.println("Nombre:" + name);
+        System.out.println("Vuelos: " + flights);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Flight[] getFlights() {
+        return flights;
+    }
+
+    public void setFlights(Flight[] flights) {
+        this.flights = flights;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Airline airline = (Airline) o;
+        return Objects.equals(name, airline.name) && Objects.deepEquals(flights, airline.flights);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, Arrays.hashCode(flights));
+    }
+
+    @Override
+    public String toString() {
+        return "Airline{" +
+                "name='" + name + '\'' +
+                ", flights=" + Arrays.toString(flights) +
+                '}';
+    }
+}
